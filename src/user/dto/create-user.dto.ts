321efaf50f89eeb757e94d423a2ef18c98f3265a
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2024-05-01 17:40:44 -0300

    Creating application and configuring prisma

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDTO {
+
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    // @IsStrongPassword({
+    //     minLength: 6,
+    //     minUppercase: 0,
+    //     minLowercase: 0,
+    //     minNumbers: 0,
+    //     minSymbols: 0,
+    // })
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+}
