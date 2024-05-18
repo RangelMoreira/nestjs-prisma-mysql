4940bccb299418058210ec52e669cb713ce5b3b5
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2024-05-18 14:05:49 -0300

    Adding interceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Get, Param, Post, Put, Patch, Delete, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put, Patch, Delete, ParseIntPipe, UseInterceptors } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
 import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 import { UserService } from "./user.service";
+import { LogInterceptor } from "src/interceptors/log.interceptor";
 
+
+@UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
 
     constructor(private readonly usersService: UserService) { }
+
 
     @Post()
     async create(@Body() data: CreateUserDTO) {
